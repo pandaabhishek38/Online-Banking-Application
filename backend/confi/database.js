f98daaae13c48973833fbfdb56c459f2e0d570e1
pandaabhishek38 <56110423+pandaabhishek38@users.noreply.github.com>
2024-03-01 17:10:14 -0500

    Add files via upload

---
+++ backend/confi/database.js
@@ -0,0 +1,18 @@
+const mysql=require('mysql');
+require('dotenv').config();
+const dbConfig={
+    host:'127.0.0.1',
+    user:'root',
+    password: 'Bunty@7931',
+    database:'bank'
+};
+
+const connection = mysql.createConnection(dbConfig);
+connection.connect((err)=>{
+    if(err){
+        console.error('Error connecting to database:',err);
+    }else{
+        console.log('Connected to the database');
+    }
+});
+module.exports=connection;
